b4f9b3f8097445af7ef5e48eec47af1a15bab890
Miguel Bastos <mig.silva@gmail.com>
2022-03-24 23:17:41 +0000

    simplify

---
+++ src/mars-rover.test.ts
@@ -0,0 +1,25 @@
+import { marsRover } from "./mars-rover";
+
+describe("[mars-rover]", () => {
+  describe.each`
+    commands     | expectedPosition
+    ${"L"}       | ${"0:0:W"}
+    ${"LL"}      | ${"0:0:S"}
+    ${"LLL"}     | ${"0:0:E"}
+    ${"LLLL"}    | ${"0:0:N"}
+    ${"R"}       | ${"0:0:E"}
+    ${"RR"}      | ${"0:0:S"}
+    ${"RRR"}     | ${"0:0:W"}
+    ${"RRRR"}    | ${"0:0:N"}
+    ${"M"}       | ${"0:1:N"}
+    ${"MM"}      | ${"0:2:N"}
+    ${"MMRMMLM"} | ${"2:3:N"}
+  `("when receive command $commands", ({ commands, expectedPosition }) => {
+    it(`should move to ${expectedPosition}`, () => {
+      const rover = marsRover();
+      const run = rover.execute(commands);
+
+      expect(run).toBe(expectedPosition);
+    });
+  });
+});
---
+++ src/mars-rover.ts
@@ -0,0 +1,49 @@
+interface Rover {
+  x: number;
+  y: number;
+  direction: "N" | "E" | "S" | "W";
+}
+
+interface MarsRover {
+  execute: (commands: string) => string;
+}
+
+function rotateRight(rover: Rover): Rover {
+  if (rover.direction === "N") return { ...rover, direction: "E" };
+  if (rover.direction === "E") return { ...rover, direction: "S" };
+  if (rover.direction === "S") return { ...rover, direction: "W" };
+  return { ...rover, direction: "N" };
+}
+
+function rotateLeft(rover: Rover): Rover {
+  if (rover.direction === "N") return { ...rover, direction: "W" };
+  if (rover.direction === "W") return { ...rover, direction: "S" };
+  if (rover.direction === "S") return { ...rover, direction: "E" };
+  return { ...rover, direction: "N" };
+}
+
+function move(rover: Rover): Rover {
+  if (rover.direction === "N") return { ...rover, y: rover.y + 1 };
+  if (rover.direction === "S") return { ...rover, y: rover.y - 1 };
+  if (rover.direction === "W") return { ...rover, x: rover.x - 1 };
+  return { ...rover, x: rover.x + 1 };
+}
+
+export const marsRover = (): MarsRover => {
+  const execute = (commands: string): string => {
+    let rover: Rover = {
+      x: 0,
+      y: 0,
+      direction: "N"
+    };
+    commands.split("").forEach(command => {
+      if (command === "R") rover = rotateRight(rover);
+      if (command === "L") rover = rotateLeft(rover);
+      if (command === "M") rover = move(rover);
+    });
+    return `${rover.x}:${rover.y}:${rover.direction}`;
+  };
+  return {
+    execute
+  };
+};
